8b3ae5ea49de33d8009ccafd16eb87b123ae43bb
Chunxiao523 <zhangchunxiao523@gmail.com>
2016-09-28 01:54:36 -0500

    fadfa
    fdaa

---
+++ inclass-11/src/reducers.js
@@ -0,0 +1,27 @@
+
+const Reducer = (state =  {
+	nextId: 2,
+	todoItems: [
+	    {id: 0, text: "This is an item", done: false},
+	    {id: 1, text: "Another item", done: false}
+	]
+}, action) => {
+	switch(action.type) {
+		case 'ADD_TODO':
+			 var copy = Object.assign({}, state, {nextId: state.nextId ++},{todoItems: [...state.todoItems,{id: state.nextId, text: action.text, done: false}]} );
+			return copy
+		case 'REMOVE_TODO':
+			return Object.assign({}, state, {todoItems: state.todoItems.filter(todo => 
+				todo.id !== action.id
+				)})
+		case 'TOGGLE_TODO':
+		// var toggle = Object.assign({}, state, {todoItems: } );
+		// 	return toggle
+			// state.todoItems[action.id].done = true;
+			// return state
+		default: 
+			return state
+	}
+}
+
+export default Reducer
